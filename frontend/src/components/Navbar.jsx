e2c648c60e128b5268889bc9d0164d984c54cee6
Aditya77777 <adityakardile.skn.entc@gmail.com>
2026-06-27 12:26:49 +0530

    fix Hire Me mailto

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,6 +6,23 @@ import { Routes, Route } from "react-router-dom";
 const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
+
+  
+    const handleHireMe = () => {
+      const subject = "Job Opportunity / Project Inquiry";
+      const body = 
+        `Hi Aditya,
+        I came across your portfolio and would like to discuss a potential opportunity / project with you.
+        Looking forward to hearing from you.
+        Best regards,`;
+
+      const mailtoUrl = `mailto:adityakardile77@gmail.com?subject=${encodeURIComponent(
+        subject
+      )}&body=${encodeURIComponent(body)}`;
+
+      window.location.href = mailtoUrl;
+    };
+
   return (
     <div className="bg-white rounded-2xl p-4 flex items-center justify-between shadow-sm">
       {/* Left Icons */}
@@ -39,6 +56,7 @@ const Navbar = () => {
       {/* Hire Me Button */}
       <button className="bg-black text-white px-5 py-3 rounded-xl flex items-center gap-2 font-semibold shadow-md">
         <IoAddCircleOutline className="text-lg" />
+        onClick={handleHireMe}
         Hire Me
       </button>
     </div>
